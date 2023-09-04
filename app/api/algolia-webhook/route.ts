b6d65e0df95f45db7c7f76a9b62dc66002d81baa
Pierniki <jjdroid6969@gmail.com>
2023-09-04 14:30:01 +0200

    feat: add algolia webook api route

---
+++ app/api/algolia-webhook/route.ts
@@ -0,0 +1,31 @@
+import algolia from "algoliasearch"
+import { env } from "env.mjs"
+import { NextRequest, NextResponse } from "next/server"
+import { z } from "zod"
+
+const client = algolia(env.ALGOLIA_API_ID, env.ALGOLIA_API_KEY)
+const index = client.initIndex("articles")
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const authHeader = req.headers.get("authorization")
+  if (!authHeader || authHeader !== env.HYGRAPH_WEBOOK_SECRET)
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
+  try {
+    const publishedData = await req.json()
+
+    const parseResult = bodySchema.safeParse(publishedData)
+    if (!parseResult.success) return NextResponse.json({ message: "Bad Request" }, { status: 400 })
+
+    const { id: objectID, ...data } = parseResult.data.data.PUBLISHED
+
+    await index.saveObject({ objectID, ...data })
+
+    return NextResponse.json({ message: "ok" }, { status: 201 })
+  } catch (err) {
+    return NextResponse.json({ message: "Unexpected Error" }, { status: 500 })
+  }
+}
+
+const bodySchema = z.object({
+  data: z.object({ PUBLISHED: z.record(z.string(), z.any()).and(z.object({ id: z.string() })) }),
+})
--- env.mjs
+++ env.mjs
@@ -8,10 +8,16 @@ export const env = createEnv({
       .optional()
       .transform((value) => value === "true"),
     HYGRAPH_CONTENT_API_URL: z.string(),
+    HYGRAPH_WEBOOK_SECRET: z.string(),
+    ALGOLIA_API_ID: z.string(),
+    ALGOLIA_API_KEY: z.string(),
   },
   client: {},
   runtimeEnv: {
     ANALYZE: process.env.ANALYZE,
     HYGRAPH_CONTENT_API_URL: process.env.HYGRAPH_CONTENT_API_URL,
+    HYGRAPH_WEBOOK_SECRET: process.env.HYGRAPH_WEBOOK_SECRET,
+    ALGOLIA_API_ID: process.env.ALGOLIA_API_ID,
+    ALGOLIA_API_KEY: process.env.ALGOLIA_API_KEY,
   },
 })
