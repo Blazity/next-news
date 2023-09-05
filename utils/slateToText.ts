8d41ba91126aba72b3c09aa46f9ea1a55cccaa98
Pierniki <jjdroid6969@gmail.com>
2023-09-05 12:24:38 +0200

    fix: only merge text values in slateToText function

--- utils/slateToText.ts
+++ utils/slateToText.ts
@@ -1,10 +1,14 @@
 import { isArray, isObject, isString, values } from "lodash"
 
 export function slateToText(obj: unknown): string {
-  if (isObject(obj))
+  if (isObject(obj)) {
+    if ("text" in obj && typeof obj["text"] === "string") {
+      return obj["text"]
+    }
     return values(obj)
       .map(slateToText)
       .reduce((acc, val) => acc + val, "")
+  }
   if (isArray(obj)) return obj.map(slateToText).reduce((acc, val) => acc + val, "")
   if (isString(obj)) return obj
   return ""
