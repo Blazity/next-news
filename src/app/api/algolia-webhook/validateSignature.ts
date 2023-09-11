2f3b7243616e0279de65d0a929f97691f50df2f7
Pierniki <jjdroid6969@gmail.com>
2023-09-11 13:25:35 +0200

    feat: implement edge compatible verifyWebhookSignature function

--- src/app/api/algolia-webhook/validateSignature.ts
+++ src/app/api/algolia-webhook/validateSignature.ts
@@ -1,6 +1,5 @@
 import { NextRequest } from "next/server"
 import { HttpError } from "./httpError"
-import { verifyWebhookSignature } from "@hygraph/utils"
 import { env } from "../../../env.mjs"
 
 export const validateSignature = async (req: NextRequest) => {
@@ -23,3 +22,38 @@ export type NextRequestWithBody = NextRequest & { parsedBody: unknown }
 
 export const hasParsedBody = (req: NextRequestWithBody | NextRequest): req is NextRequestWithBody =>
   Boolean((req as NextRequestWithBody).body)
+
+async function verifyWebhookSignature({
+  body,
+  signature,
+  secret,
+  rawPayload,
+}: {
+  body?: any
+  signature: string
+  secret: string
+  rawPayload?: string
+}) {
+  const [rawSign, rawEnv, rawTimestamp] = signature.split(", ")
+
+  const sign = rawSign.replace("sign=", "")
+  const EnvironmentName = rawEnv.replace("env=", "")
+  const Timestamp = parseInt(rawTimestamp.replace("t=", ""))
+
+  let payload = JSON.stringify({
+    Body: rawPayload || JSON.stringify(body),
+    EnvironmentName,
+    TimeStamp: Timestamp,
+  })
+
+  const encoder = new TextEncoder()
+  const key = await crypto.subtle.importKey("raw", encoder.encode(secret), { name: "HMAC", hash: "SHA-256" }, false, [
+    "sign",
+  ])
+
+  const signatureArrayBuffer = await crypto.subtle.sign("HMAC", key, encoder.encode(payload))
+
+  const hash = btoa(String.fromCharCode(...Array.from(new Uint8Array(signatureArrayBuffer))))
+
+  return sign === hash
+}
