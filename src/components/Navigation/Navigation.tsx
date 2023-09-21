93a8ddea356816dddbf3324f1363419b8e3a48a4
Max Mogilski <maksmogi@gmail.com>
2023-09-21 14:30:01 +0200

    fix: query fix - nav

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -6,26 +6,16 @@ type NavigationProps = {
   lang: Locale
 }
 
-type Page = {
-  __typename?: "Page"
-  slug: string
-  title: string
-}
-
-type NavItem = {
-  pages: Page[]
-}
-
 export async function Navigation({ lang }: NavigationProps) {
   const { getNav } = HygraphApi({ lang })
-  const { nav } = await getNav({})
-  const navElements = nav.reduce((acc: Page[], curr: NavItem) => acc.concat(curr.pages), [])
+  const { navigations } = await getNav({})
+  const navElements = navigations[0]?.pages
 
   return (
     <ul className="flex items-center justify-center gap-5 px-4">
-      {navElements.map((navElement) => (
-        <li key={navElement.slug}>
-          <Link href={`/${lang}/${navElement.slug}`}>{navElement.title}</Link>
+      {navElements?.map((navElement) => (
+        <li key={navElement?.slug}>
+          <Link href={`/${lang}/${navElement?.slug}`}>{navElement?.title}</Link>
         </li>
       ))}
     </ul>
