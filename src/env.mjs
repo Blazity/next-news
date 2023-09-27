f38fe7f617731eb7f1550b2010d925c431eb61e2
Pierniki <jjdroid6969@gmail.com>
2023-09-27 14:49:53 +0200

    ci: log SKIP_VALIDATION variable

--- src/env.mjs
+++ src/env.mjs
@@ -1,6 +1,7 @@
 import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
+console.log("SKIP VALIDATION", process.env.SKIP_ENV_VALIDATION)
 export const env = createEnv({
   server: {
     ANALYZE: z
