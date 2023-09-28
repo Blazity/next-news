c0c127fd377737ee1179de6e8d1592a24f43a9b7
Pierniki <jjdroid6969@gmail.com>
2023-09-28 10:49:17 +0200

    ci: replace GA email and key with base64 encoded json, fix yml file

--- src/components/TrendingArticles/getTrendingArticles.ts
+++ src/components/TrendingArticles/getTrendingArticles.ts
@@ -1,19 +1,17 @@
 import { BetaAnalyticsDataClient } from "@google-analytics/data"
 import keyBy from "lodash/keyBy"
 import sortBy from "lodash/sortBy"
+import { z } from "zod"
 import { env } from "@/env.mjs"
 import { Locale } from "@/i18n/i18n"
 import { listArticlesBySlugs } from "@/lib/client"
 import { pipe } from "@/utils/pipe"
 import { getConfig } from "./reportConfig"
 
-console.log("GA", env.GA_EMAIL, env.GA_PRIVATE_KEY)
+const credentials = JSON.parse(atob(env.GA_BASE64_SERVICE_ACCOUNT))
 
 const analyticsDataClient = new BetaAnalyticsDataClient({
-  credentials: {
-    client_email: env.GA_EMAIL,
-    private_key: env.GA_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-  },
+  credentials: z.object({ client_email: z.string(), private_key: z.string() }).parse(credentials),
 })
 
 async function runReport(locale: string) {
