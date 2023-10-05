b5bfdb35bb1f0f5cc9ec294560f8fd22a4270351
Max Mogilski <maksmogi@gmail.com>
2023-10-05 13:59:45 +0200

    feat: add recommended articles styles

--- src/components/RecommendedArticles/RecommendedArticles.tsx
+++ src/components/RecommendedArticles/RecommendedArticles.tsx
@@ -7,6 +7,7 @@ type imageInfo = {
 }
 
 export type RecommendedArticle = {
+  tags: string[]
   title: string
   slug: string
   id: string
@@ -17,8 +18,8 @@ type RecommendedArticlesProps = { recommendedArticles: RecommendedArticle[]; lan
 
 export async function RecommendedArticles({ recommendedArticles, lang }: RecommendedArticlesProps) {
   return (
-    <section className="w-full px-4">
-      <h2 className="mb-4 text-2xl font-bold">Recommended articles</h2>
+    <section className="w-full py-4">
+      <h2 className="mb-8 text-2xl font-bold">Related articles</h2>
       <ArticlesGrid locale={lang} articles={recommendedArticles} />
     </section>
   )
