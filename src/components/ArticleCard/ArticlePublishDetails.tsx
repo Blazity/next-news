a8a44d466c15be35886f6e2073e5e94b6945c91a
Max Mogilski <maksmogi@gmail.com>
2023-10-09 09:37:15 +0200

    fix: display author details issue

--- src/components/ArticleCard/ArticlePublishDetails.tsx
+++ src/components/ArticleCard/ArticlePublishDetails.tsx
@@ -20,7 +20,7 @@ export function ArticlePublishDetails({
       className={cn(
         variant === "dark" && " text-custom-dim",
         variant === "light" && " text-gray-500",
-        "flex items-center gap-2 text-center text-sm "
+        "flex items-center gap-2 whitespace-nowrap text-center text-sm"
       )}
       style={{ textShadow: variant === "dark" ? "0px 1px 2px rgba(26, 26, 27, 1)" : undefined }}
     >
