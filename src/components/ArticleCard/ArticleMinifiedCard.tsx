abf6d3f1c44609cf77d4da000017165c7d752f62
Pierniki <jjdroid6969@gmail.com>
2023-10-09 10:25:42 +0200

    fix: minified card width

--- src/components/ArticleCard/ArticleMinifiedCard.tsx
+++ src/components/ArticleCard/ArticleMinifiedCard.tsx
@@ -15,11 +15,11 @@ export function ArticleMinifiedCard({ article: { imageUrl, title, slug }, locale
   return (
     <Link href={`/${locale}/article/${slug}`} hrefLang={locale} passHref>
       <article className="flex w-full gap-5">
-        <div className="relative h-[82px] w-[82px] rounded-xl bg-gradient-to-br from-gray-200 to-gray-300">
+        <div className="relative h-[82px] min-w-[82px] rounded-xl bg-gradient-to-br from-gray-200 to-gray-300">
           {imageUrl && (
             <Image
               src={imageUrl}
-              alt="test"
+              alt={title}
               width={82}
               height={82}
               className=" min-h-[82px] rounded-xl object-cover text-center brightness-90"
