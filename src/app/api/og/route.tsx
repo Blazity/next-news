607972cbaf3908bdd45d96ddaf09259a91f8312e
Pierniki <jjdroid6969@gmail.com>
2023-10-10 10:42:48 +0200

    fix: update the way of retrieving search params
    \

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -5,7 +5,7 @@ import { ImageResponse, NextRequest } from "next/server"
 export const runtime = "edge"
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url)
+  const searchParams = request.nextUrl.searchParams
   const image = searchParams.get("image")
   const title = searchParams.get("title")
 
