cdc333c6d7d2961c4ee8892a8ccfc63e98b1c0c6
Pierniki <jjdroid6969@gmail.com>
2023-10-13 14:44:03 +0200

    feat: move layout up a level

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,19 @@
 import { Footer } from "@/components/Footer/Footer"
 import { Navigation } from "@/components/Navigation/Navigation"
 import { env } from "@/env.mjs"
-import { type Locale } from "@/i18n/i18n"
+import { i18n, type Locale } from "@/i18n/i18n"
 import "@/styles/tailwind.css"
+
+import { getNavigation } from "@/lib/client"
 import { GoogleAnalytics } from "./GoogleAnalytics"
 import Providers from "./Providers"
-import { getNavigation } from "@/lib/client"
 
 export async function generateMetadata({ params }: { params: { lang: Locale } }) {
+  const locale = params.lang ?? i18n.defaultLocale
   return {
     title: "Blazity-Hygraph news starter",
     openGraph: {
-      url: "https://next-enterprise.vercel.app/",
+      url: env.NEXT_PUBLIC_SITE_URL,
       images: [
         {
           url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/project-logo.png",
@@ -25,28 +27,29 @@ export async function generateMetadata({ params }: { params: { lang: Locale } })
     },
     alternates: {
       types: {
-        "application/rss+xml": `${env.NEXT_PUBLIC_SITE_URL}/api/${params?.lang}`,
+        "application/rss+xml": `${env.NEXT_PUBLIC_SITE_URL}/api/${locale}`,
       },
     },
   }
 }
 
-export default async function Layout({ children, params }: { children: React.ReactNode; params: { lang: Locale } }) {
-  const navigation = await getNavigation(params.lang)
+export default async function Layout({ children, params }: { children: React.ReactNode; params: { lang?: Locale } }) {
+  const locale = params.lang ?? i18n.defaultLocale
+  const navigation = await getNavigation(locale)
 
   return (
-    <html lang={params.lang}>
+    <html lang={locale}>
       <GoogleAnalytics />
       <Providers>
         <body className="flex min-h-screen flex-col items-center ">
           <div className="z-50 flex w-full justify-center border-b bg-white">
             <nav className="flex w-full max-w-[1200px] items-center justify-end gap-4 py-4">
-              <Navigation navigation={navigation} locale={params.lang} />
+              <Navigation navigation={navigation} locale={locale} />
             </nav>
           </div>
 
           <main className="mx-auto flex w-full max-w-[1200px] flex-1 flex-col px-4 pb-16">{children}</main>
-          <Footer lang={params.lang} />
+          <Footer lang={locale} />
         </body>
       </Providers>
     </html>
