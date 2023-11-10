f389a15f0b4fdf1cd6988f3bbd3be1e01f9fb26b
Max Mogilski <maksmogi@gmail.com>
2023-11-10 10:49:31 +0100

    fix: quote color tweak

--- src/components/StockDisplay/StockQuote.tsx
+++ src/components/StockDisplay/StockQuote.tsx
@@ -7,7 +7,7 @@ export function StockQuote({ quote }: { quote: Quote }) {
   return (
     <div className="flex items-center justify-center gap-4 border-r-2 border-slate-100 px-10 text-sm">
       <span className="whitespace-nowrap font-medium">{quote.name}</span>
-      <span className={cn("flex items-center gap-1", isNegative ? "text-red-800" : "text-green-800")}>
+      <span className={cn("flex items-center gap-1", isNegative ? "text-custom-red" : "text-custom-green")}>
         {!isNegative && "+"}
         {quote.change.toFixed(2)}%
         {isNegative ? <MoveDown className="mt-0.5 h-3 w-3" /> : <MoveUp className="mt-0.5  h-3 w-3" />}
